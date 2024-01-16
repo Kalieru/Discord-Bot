a5564c4473ea18ab05b8bef5c2d506152cb9195c
Kalieru <124403971+Kalieru@users.noreply.github.com>
2024-01-16 21:38:02 +0100

    Update Lingo.py

--- Lingo.py
+++ Lingo.py
@@ -1,25 +1,25 @@
 def link(n):
         if n <= 5: 
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\5.txt"
+            return r"5.txt"
         if n == 6:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\6.txt"
+            return r"6.txt"
         if n == 7:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\7.txt"
+            return r"7.txt"
         if n == 8:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\8.txt"
+            return r"8.txt"
         if n == 9:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\9.txt"
+            return r"9.txt"
         if n == 10:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\10.txt"
+            return r"10.txt"
         if n == 11:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\11.txt"
+            return r"11.txt"
         if n == 12:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\12.txt"
+            return r"12.txt"
         if n == 13:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\13.txt"
+            return r"13.txt"
         if n == 14:
-            return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\14.txt"
-        return r"C:\Users\danie\OneDrive\Escritorio\Python\Projectos\Lingo\Texti\15.txt"
+            return r"14.txt"
+        return r"15.txt"
 def isAnagram(word, c):
     for letter in word:
         if word.count(letter) != c.count(letter):
